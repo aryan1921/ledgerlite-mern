bbc94bb20723eb8e3b4d7bd2e90606d19ec35fe1
aryankaushik21 <aryankaushik207@gmail.com>
2025-08-15 17:40:03 +0530

    added the User model for mongoose

---
+++ server/src/models/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true, index: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.compare = function(pw) {
+  return bcrypt.compare(pw, this.password);
+};
+
+export default mongoose.model("User", UserSchema);
